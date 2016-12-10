1fa20eb7497b4dd99f0427b40fec8d14db72d700
KonH <konh@yandex.ru>
2016-12-10 18:58:35 +0700

    Characters assets and fixes;

---
+++ Assets/Scripts/Behaviours/EnemyAttackAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EnemyAttackAnimation : MonoBehaviour {
+
+	public float Duration;
+	public Transform Left;
+	public Transform Right;
+
+	Sequence _seq;
+
+	public void Animate() {
+		if( _seq != null ) {
+			_seq.Complete();
+			_seq.Kill();
+			_seq = null;
+		}
+		_seq = DOTween.Sequence();
+		var leftOriginal = Left.localEulerAngles;
+		var rightOriginal = Right.localEulerAngles;
+		_seq.Append(Left.DOLocalRotate(Vector3.zero, Duration));
+		_seq.Insert(0, Right.DOLocalRotate(Vector3.zero, Duration));
+		_seq.Append(Left.DOLocalRotate(leftOriginal, Duration));
+		_seq.Insert(Duration, Right.DOLocalRotate(rightOriginal, Duration));
+	}
+}
