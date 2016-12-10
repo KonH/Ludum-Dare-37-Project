f913b3e55973b418a520e1f102eb47b2c9643b62
KonH <konh@yandex.ru>
2016-12-10 19:36:00 +0700

    Effects and fixes;

--- Assets/Scripts/Behaviours/ScaleTween.cs
+++ Assets/Scripts/Behaviours/ScaleTween.cs
@@ -8,13 +8,27 @@ public class ScaleTween : ObjectTween {
 	public float Duration = 0.5f;
 
 	public override void OnShow() {
+		var collider = GetComponent<Collider>();
+		if( collider ) {
+			collider.enabled = false;
+		}
 		var scale = transform.localScale;
 		transform.localScale = Vector3.zero;
 		transform.DOScale(scale, Duration).
-		SetEase(Ease.InElastic);
+		SetEase(Ease.InElastic).
+		OnComplete(() => {
+			if( collider ) { 
+				collider.enabled = true; 
+			}
+		}
+		);
 	}
 
 	public override void OnHide() {
+		var collider = GetComponent<Collider>();
+		if( collider ) {
+			collider.enabled = false;
+		}
 		transform.DOScale(0, Duration).
 		SetEase(Ease.InElastic).
 		OnComplete(() => gameObject.SetActive(false));
--- Assets/Scripts/Spells/BoomSpell.cs
+++ Assets/Scripts/Spells/BoomSpell.cs
@@ -8,12 +8,19 @@ public class BoomSpell : MonoBehaviour {
 	public float SizeTime;
 
 	float _startTime;
+	Light _light;
 
 	void Awake() {
+		_light = GetComponentInChildren<Light>();
 		_startTime = Time.time;
 		var endScale = transform.localScale;
 		transform.localScale = Vector3.zero;
-		transform.DOScale(endScale, SizeTime).OnComplete(() => gameObject.SetActive(false));
+		transform.DOScale(endScale, SizeTime).
+		SetEase(Ease.OutBounce).
+		OnComplete(() => OnComplete());
+		var endIntensity = _light.intensity;
+		_light.intensity = 0;
+		_light.DOIntensity(endIntensity, SizeTime);
 	}
 		
 	void OnCollisionEnter(Collision other) {
@@ -23,5 +30,11 @@ public class BoomSpell : MonoBehaviour {
 		if( destroyable ) {
 			destroyable.GiveDamage(MaxDamage * coeff);
 		}
+	}
+
+	void OnComplete() {
+		GetComponent<Collider>().enabled = false;
+		transform.DOScale(0, SizeTime/2);
+		_light.DOIntensity(0, SizeTime/3);
 	}
 }
