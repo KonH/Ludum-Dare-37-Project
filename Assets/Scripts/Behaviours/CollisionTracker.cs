82f48ef97541e2a12937a8708fed764d27572302
KonH <konh@yandex.ru>
2016-12-11 17:23:25 +0700

    Set front wall transparent if player move near it;

--- Assets/Scripts/Behaviours/CollisionTracker.cs
+++ Assets/Scripts/Behaviours/CollisionTracker.cs
@@ -5,11 +5,18 @@ using UnityEngine.Events;
 
 public class CollisionTracker : MonoBehaviour {
 	public UnityEvent OnEnter;
+	public UnityEvent OnExit;
 	public string TagFilter;
 
 	void OnTriggerEnter(Collider other) {
-		if( other.GetComponent<Collider>().tag == TagFilter ) {
+		if( other.tag == TagFilter ) {
 			OnEnter.Invoke();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if( other.tag == TagFilter ) {
+			OnExit.Invoke();
 		}
 	}
 }
